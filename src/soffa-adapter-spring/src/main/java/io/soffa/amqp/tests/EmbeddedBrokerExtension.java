3d6e76bc53eba90523a5d5b87335520fc9d48214
Titus Gahissy <titus@gahissy.com>
2019-10-05 21:59:21 +0100

    Make embedded broker disable

--- src/soffa-adapter-spring/src/main/java/io/soffa/amqp/tests/EmbeddedBrokerExtension.java
+++ src/soffa-adapter-spring/src/main/java/io/soffa/amqp/tests/EmbeddedBrokerExtension.java
@@ -1,5 +1,6 @@
 package io.soffa.amqp.tests;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -7,16 +8,20 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 public class EmbeddedBrokerExtension implements BeforeAllCallback, AfterAllCallback {
 
     public static String DEFAULT_QUEUE_NAME = "default";
+    private static final boolean disabled = StringUtils.trimToEmpty(System.getenv("EMBEDDED_TEST_BROKER")).equals("false");
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
+        if (disabled) {
+            return;
+        }
         EmbeddedMQ.boostrap(DEFAULT_QUEUE_NAME);
         Thread.sleep(100);
     }
 
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
+        if (disabled) return;
         EmbeddedMQ.shutdown();
-
     }
 }
