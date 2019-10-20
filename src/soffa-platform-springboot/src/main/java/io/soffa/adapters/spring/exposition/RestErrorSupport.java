0b42231739faeaccb6f73375a3a1995a428628f5
Titus Gahissy <titus@gahissy.com>
2019-10-20 16:42:43 +0100

    Use default error attributes

--- src/soffa-platform-springboot/src/main/java/io/soffa/adapters/spring/exposition/RestErrorSupport.java
+++ src/soffa-platform-springboot/src/main/java/io/soffa/adapters/spring/exposition/RestErrorSupport.java
@@ -2,24 +2,21 @@ package io.soffa.adapters.spring.exposition;
 
 import io.soffa.core.security.InvalidAuthException;
 import org.apache.http.HttpStatus;
-
 import java.lang.reflect.UndeclaredThrowableException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class RestErrorSupport {
 
-    public static Map<String, Object> getErrorAttributes(Throwable ex) {
+    public static Map<String, Object> getErrorAttributes(Map<String, Object> attributes, Throwable ex) {
         String message = ex.getMessage();
-        Map<String, Object> map = new HashMap<>();
         if (ex instanceof UndeclaredThrowableException) {
             message = ex.getCause().getMessage();
             if (ex.getCause() instanceof InvalidAuthException) {
-                map.put("status", HttpStatus.SC_FORBIDDEN);
+                attributes.put("status", HttpStatus.SC_FORBIDDEN);
             }
         }
-        map.put("message", message);
-        return map;
+        attributes.put("message", message);
+        return attributes;
     }
 
 }
