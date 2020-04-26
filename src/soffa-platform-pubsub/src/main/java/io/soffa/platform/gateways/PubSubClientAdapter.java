3ab0fca6376e823842ffc98893fcb9ac0bf39855
Titus Gahissy <titus@gahissy.com>
2020-04-26 19:31:13 +0000

    Refactoring

---
+++ src/soffa-platform-pubsub/src/main/java/io/soffa/platform/gateways/PubSubClientAdapter.java
@@ -0,0 +1,51 @@
+package io.soffa.platform.gateways;
+
+import io.soffa.platform.core.data.JSON;
+import io.soffa.platform.core.pubsub.Event;
+import io.soffa.platform.core.pubsub.PubSubClient;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class PubSubClientAdapter implements PubSubClient {
+
+    public static final String DEFAULT_ROUTING_KEY = "default";
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public PubSubClientAdapter(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void send(Event event) {
+        rabbitTemplate.convertAndSend(applicationName, DEFAULT_ROUTING_KEY, JSON.serializeSafe(event).getBytes());
+    }
+
+    @Override
+    public void send(String channel, Event event) {
+        rabbitTemplate.convertAndSend(channel, DEFAULT_ROUTING_KEY, JSON.serializeSafe(event).getBytes());
+    }
+
+    @Override
+    public void sendDelayed(String channel, Event event, int ttlInSeconds) {
+        rabbitTemplate.convertAndSend(channel, DEFAULT_ROUTING_KEY, JSON.serializeSafe(event).getBytes(), message -> {
+            message.getMessageProperties().setHeader("x-delay", TimeUnit.SECONDS.toMillis(ttlInSeconds));
+            return message;
+        });
+    }
+
+    @Override
+    public void sendDelayed(Event event, int ttlInSeconds) {
+        rabbitTemplate.convertAndSend(applicationName, DEFAULT_ROUTING_KEY, JSON.serializeSafe(event).getBytes(), message -> {
+            message.getMessageProperties().setHeader("x-delay", TimeUnit.SECONDS.toMillis(ttlInSeconds));
+            return message;
+        });
+    }
+}
